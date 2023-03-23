2b52614c2eabb5c62f81dac6d9d3b37240258996
Daniel Wagner-Hall <dwagnerhall@apple.com>
2023-03-23 17:27:55 +0000

    Change linux src

--- java/example/simple/SimpleLinux.java
+++ java/example/simple/SimpleLinux.java
@@ -2,6 +2,6 @@ package example.simple;
 
 public class SimpleLinux {
   public static String description() {
-    return String.format("I am a simple class named SimpleLinux, and I know about %s", LinuxDep.NAME);
+    return String.format("I am a simple class named SimpleLinux, and I know about %s - changed", LinuxDep.NAME);
   }
 }
