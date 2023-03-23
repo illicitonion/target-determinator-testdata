9aac49274c8e79be8b50e28bc028e44c022476ef
Daniel Wagner-Hall <dwagnerhall@apple.com>
2023-03-23 17:27:40 +0000

    Change non-linux src

--- java/example/simple/Simple.java
+++ java/example/simple/Simple.java
@@ -2,6 +2,6 @@ package example.simple;
 
 public class Simple {
   public static String description() {
-    return String.format("I am a simple class named Simple, and I know about %s", Dep.NAME);
+    return String.format("I am a simple class named Simple, and I know about %s - changed", Dep.NAME);
   }
 }
